d98ad381fcf3982196792ce977bde8f40b3c224b
GustavoZLongo <gustavoz.longo@hotmail.com>
2017-08-31 20:48:05 -0300

    teste

---
+++ teste/README.md
@@ -0,0 +1,26 @@
+# Exercícios
+
+Entrega até 17/08/2017
+
+1. Faça um programa em C# que imprima a soma dos números de 1 até 1000.
+
+2. Faça um programa em C# que imprima todos os múltiplos de 3, entre 1 e 100
+
+3. Faça um programa em C# que imprima os fatoriais de 1 a 10
+
+4. Faça um programa que imprima a seguinte tabela, usando fors encadeados:
+    1
+
+    2 4
+
+    3 6 9
+
+    4 8 12 16
+
+    n n*2 n*3 .... n*n
+
+## Desafios
+
+1. Crie um programa C# que imprima os 10 primeiros números da sequência de Fibonacci. (uma sequência de números inteiros, começando normalmente por 0 e 1, na qual, cada termo subsequente corresponde a soma dos dois anteriores) (0,1, 1, 2, 3, 5, 8, 13, 21, 34)
+
+2. Crie um programa C# que imprima uma matriz triangular superior de números 1 6x6
---
+++ teste/exemplos/ExercicioTeste/ExercicioTeste/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite um numero");
+            int x = int.Parse(Console.ReadLine());
+
+            int[] vetor = { 1, 3, 5, 7, 4 };
+
+            //imprimir o vetor
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                Console.WriteLine(vetor[i]);
+            }
+
+            foreach (int i in vetor)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            Console.WriteLine("A tabuada é:");
+
+            for (int i = 0; i <= 10; i++)
+            {
+                //Console.WriteLine(x +"x" + i + "=" + (x*i));
+                Console.WriteLine($"{x} x {i} = {x * i}");
+
+            }
+
+            Console.WriteLine("\rA tabuada dos pares é:");
+
+            for (int i = 0; i <= 10; i++)
+            {
+                if ((x * i) % 2 == 0)
+                {
+                    Console.WriteLine($"{x} x {i} = {x * i}");
+
+                }
+
+            }
+
+            //imprime se um numero é positivo ou negativo
+            string resultado = x >= 0 ? "Positivo" : "Negativo";
+            Console.WriteLine(resultado);
+
+            Console.ReadKey();
+        }
+    }
+}
